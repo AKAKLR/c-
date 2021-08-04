e830c3d17d7be3d5b77f46a1ca1283328c9a667d
AKAKLR <2236564424@qq.com>
2021-08-04 22:32:25 +0800

    8.4

--- 源.cpp
+++ 源.cpp
@@ -1,24 +1,44 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
 #include <stdio.h>
 #include <stdlib.h>
-#include <assert.h>
 
-char* reverse_string(char* str)
+int D_jump_floor(int n)//递归
 {
-    assert(str != NULL);
-    if (*str != '\0')
-    {
-        str++;
-        reverse_string(str);
-        printf("%c", *(str - 1));
-    }
-    return str;
+	if (n <= 2)
+	{
+		return n;
+	}
+	else
+	{
+		return D_jump_floor(n - 2) + D_jump_floor(n - 1);
+	}
+}
+
+int FD_jump_floor(int n)//非递归
+{
+	int a = 1;//第一个数
+	int b = 2;//第二个数
+	int c = a;//把c赋值为a
+	while (n > 2)
+	{
+		c = a + b;
+		a = b;
+		b = c;
+		n--;
+	}
+	return c;
 }
 
 int main()
 {
-    char arr[] = "abcdef";
-    char* ret = reverse_string(arr);
-    system("pause");
-    return 0;
+	int n = 0;
+	printf("请输入跳的台阶数：");
+	scanf("%d", &n);
+	int ret1 = D_jump_floor(n);
+	int ret2 = FD_jump_floor(n);
+	printf("%d\n", ret1);
+	printf("%d\n", ret2);
+	system("pause");
+	return 0;
 }
-
